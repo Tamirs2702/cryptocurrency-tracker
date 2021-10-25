719f7b08a2f329540d92c7fb8e9ecae1f51aad85
Tamir Shmueli <dojo4dojo@gmail.com>
2021-10-25 04:04:00 +0300

    rhr

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -1,45 +1,14 @@
 import React, { useRef, useEffect, useState } from "react";
-import Chartjs from "chart.js";
-import { historyOptions } from "../chartConfigs/chartConfigs";
 
 const HistoryChart = ({ data }) => {
   const chartRef = useRef();
-  const { day, week, year, detail } = data;
-  const [timeFormat, setTimeFormat] = useState("24h");
+  const { detail } = data;
+  const [, setTimeFormat] = useState("24h");
 
-  const determineTimeFormat = () => {
-    switch (timeFormat) {
-      case "24h":
-        return day;
-      case "7d":
-        return week;
-      case "1y":
-        return year;
-      default:
-        return day;
-    }
-  };
 
   useEffect(() => {
     if (chartRef && chartRef.current && detail) {
       console.log("yeah");
-      const chartInstance = new Chartjs(chartRef.current, {
-        type: "line",
-        data: {
-          datasets: [
-            {
-              label: `${detail.name} price`,
-              data: determineTimeFormat(),
-              backgroundColor: "rgba(174, 305, 194, 0.5)",
-              borderColor: "rgba(174, 305, 194, 0.4",
-              pointRadius: 0,
-            },
-          ],
-        },
-        options: {
-          ...historyOptions,
-        },
-      });
     }
   });
 
